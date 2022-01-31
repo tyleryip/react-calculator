39b998327335088a65310f3f95fe0e9c1c235bae
Tyler Yip <tyler.yip1@ucalgary.ca>
2022-01-30 17:47:04 -0700

    fix: stop the window from overflowing

--- frontend/src/components/CalculatorWindow/CalculatorWindow.tsx
+++ frontend/src/components/CalculatorWindow/CalculatorWindow.tsx
@@ -2,6 +2,8 @@ import React, { useContext } from "react";
 import { CalculatorContext } from "../../contexts/CalculatorContext";
 
 export default function CalculatorWindow() {
+  const windowMaxSize = 17;
+
   const state = useContext(CalculatorContext);
   let windowValue = "";
 
@@ -13,10 +15,11 @@ export default function CalculatorWindow() {
     windowValue = state.leftOperand;
   }
 
-  // TODO: Whatever is displayed should be truncated so the value does not go off the screen.
   return (
     <div className="rounded border-black border-2 bg-white my-1 mx-2 h-12">
-      <p className="text-right text-xl px-2 py-2">{windowValue}</p>
+      <p className="text-right text-xl px-2 py-2">
+        {windowValue.substring(0, windowMaxSize)}
+      </p>
     </div>
   );
 }
